a2c76dd49a3c2bb95468cfbdfc4c4401e23edc15
Naimul Islam <ms0413354@gmail.com>
2022-09-24 17:47:43 +0600

    Cteate store model

---
+++ back-end/Model/Store.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const { ObjectId } = mongoose.Schema.Types;
+
+const storeSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			require: [true, 'Please Provide a valid store name.'],
+			unique: true,
+			enum: {
+				values: [
+					'dhaka',
+					'chattogram',
+					'rajshahi',
+
+					'khulna',
+					'sylhet',
+					'barisal',
+					'rangpur',
+					'mymensing',
+				],
+				message: '{VALUE} is not a valid name.',
+			},
+			trim: true,
+			lowercase: true,
+		},
+		description: String,
+		status: {
+			type: String,
+			enum: ['active', 'inActive'],
+			default: 'active',
+		},
+		manager: {
+			name: String,
+			phoneNumber: String,
+			id: {
+				type: ObjectId,
+				ref: 'User',
+			},
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// Create model
+const Store = mongoose.model('Store', storeSchema);
+module.children = Store;
