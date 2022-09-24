aa61ef6813d27f421cd40b326ba28cab2e35ac25
Naimul Islam <ms0413354@gmail.com>
2022-09-24 17:34:38 +0600

    Create category model

---
+++ back-end/Model/Category.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const categorySchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			require: [true, 'Please Provide a valid brand name.'],
+			unique: true,
+			minLength: [5, 'Name shout be at least 5 letter.'],
+			maxLength: [20, 'Name is too large.'],
+			trim: true,
+			lowercase: true,
+		},
+		description: String,
+		imageUrl: {
+			type: String,
+			validate: [validator.isEmail, 'Please provide a valid email.'],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// Create model
+const Category = mongoose.model('Category', categorySchema);
+module.children = Category;
