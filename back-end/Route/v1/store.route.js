e8f00abade0f20612afc335e57822091434369de
Naimul Islam <ms0413354@gmail.com>
2022-09-27 19:00:42 +0600

    add store route

--- back-end/Route/v1/store.route.js
+++ back-end/Route/v1/store.route.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
+const storeCollaction = require('../../Collaction/product.collaction');
+
+router
+	.route('/')
+	.post(storeCollaction.postProductCollaction)
+	.get(storeCollaction.getProductsCollaction);
+
+router
+	.route('/:id')
+	.get(storeCollaction.getProductByIdCollaction)
+	.patch(storeCollaction.updateProductByIdCollaction);
 
 module.exports = router;
