31ebb84747c883fc28a94104570f6f4aba164c95
Naimul Islam <ms0413354@gmail.com>
2022-09-28 20:22:00 +0600

    Second navbar complete

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Component/Home/Home';
 import Navbar from './Component/Navbar/Navbar';
+import SecondNavbar from './Component/Navbar/SecondNavbar';
 import './sass/App/App.css';
 
 function App() {
 	return (
 		<div className="bg">
 			<Navbar />
+			<SecondNavbar />
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 			</Routes>
