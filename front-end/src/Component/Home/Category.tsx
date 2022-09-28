56e5d469a5678cc63ec6df2f1efe102421520eba
Naimul Islam <ms0413354@gmail.com>
2022-09-28 22:17:46 +0600

    add swiper

---
+++ front-end/src/Component/Home/Category.tsx
@@ -0,0 +1,11 @@
+const Category = () => {
+	return (
+		<section className=" container-fluid mt-3">
+			<div>
+				<h2>Featured Categories</h2>
+			</div>
+		</section>
+	);
+};
+
+export default Category;
--- front-end/src/Component/Home/Home.tsx
+++ front-end/src/Component/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Category from './Category';
 
 const Home = () => {
 	return (
 		<section>
 			<Banner />
+			<Category />
 		</section>
 	);
 };
