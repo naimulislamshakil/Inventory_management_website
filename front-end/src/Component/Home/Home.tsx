28c0084cdd075797e9a610d7ecf4994f67ee6aeb
Naimul Islam <ms0413354@gmail.com>
2022-09-29 23:10:50 +0600

    add dinamic title

--- front-end/src/Component/Home/Home.tsx
+++ front-end/src/Component/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { dinamicTitle } from '../../Utilites/DainamicTitle';
 import Banner from '../Banner/Banner';
 import Banner2 from '../Banner/Banner2';
 import Category from './Category';
 
 const Home = () => {
+	dinamicTitle('(Home Page)');
 	return (
 		<section>
 			<Banner />
---
+++ front-end/src/Component/Home/PopularProduct.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PopularProduct = () => {
+	return (
+		<section className="container-fluid">
+			<div>
+				<h1>Popular Products</h1>
+			</div>
+		</section>
+	);
+};
+
+export default PopularProduct;
