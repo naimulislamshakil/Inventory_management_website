2c046905a374ce76c19fbb437677b86be495e329
Naimul Islam <ms0413354@gmail.com>
2022-10-05 16:09:36 +0600

    add register again

--- front-end/src/Component/Login/Login.tsx
+++ front-end/src/Component/Login/Login.tsx
@@ -2,16 +2,12 @@ import React, { FormEvent, useState } from 'react';
 import auth from '../../firebase.config';
 import { dinamicTitle } from '../../Utilites/DainamicTitle';
 import SocialMedia from '../SocialMedia/SocialMedia';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import Loading from '../Shared/Loading';
-import { errorHandeler } from '../../Utilites/ErrorHandeler';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-	const [signInWithEmailAndPassword, user, loading, error] =
-		useSignInWithEmailAndPassword(auth);
-
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
@@ -22,27 +18,28 @@ const Login = () => {
 
 	dinamicTitle('Login Page');
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-		signInWithEmailAndPassword(email, password);
+		e.preventDefault();
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				if (userCredential.user) {
+					navigate(from, { replace: true });
+				}
+			})
+			.catch((error) => {
+				toast.error(error.message);
+			});
 	};
 
-	if (loading) {
-		return <Loading />;
-	}
-	if (error) {
-		errorHandeler(error);
-	}
-	if (user?.user) {
-		navigate(from, { replace: true });
-	}
 	return (
 		<section className="container-fluid mt-3">
-			<form className="w-75 mx-auto card p-5">
+			<form onSubmit={onSubmit} className="w-75 mx-auto card p-5">
 				<h2 className="text-center">Log In</h2>
 				<div className="mb-3">
 					<label htmlFor="exampleInputEmail1" className="form-label">
 						Email address
 					</label>
 					<input
+						onBlur={(e) => setEmail(e.target.value)}
 						type="email"
 						className="form-control"
 						id="exampleInputEmail1"
@@ -57,25 +54,29 @@ const Login = () => {
 						Password
 					</label>
 					<input
+						onBlur={(e) => setPassword(e.target.value)}
 						type="password"
 						className="form-control"
 						id="exampleInputPassword1"
 					/>
 				</div>
-				<div className="mb-3 d-flex justify-content-center">
+				<div className="mb-3 d-lg-flex justify-content-lg-center d-block">
 					<h6>You Are Not A User!</h6>
 					<Link to="/register" className="nav-link">
 						<h6 className="ms-2 text-decoration-underline text-primary">
-							Please LogIn
+							Please Register.
 						</h6>
 					</Link>
 				</div>
 				<input
 					type="submit"
 					value="Login"
-					className="btn w-25 fw-bold mx-auto"
+					className="btn w-lg-25 w-100 fw-bold mx-auto"
 					style={{ backgroundColor: 'turquoise', color: 'white' }}
 				/>
+				<div className="mt-3">
+					<SocialMedia />
+				</div>
 			</form>
 		</section>
 	);
--- front-end/src/Component/Register/Register.tsx
+++ front-end/src/Component/Register/Register.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.config';
 import SocialMedia from '../SocialMedia/SocialMedia';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import Loading from '../Shared/Loading';
-import { errorHandeler } from '../../Utilites/ErrorHandeler';
 import { toast } from 'react-toastify';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Register = () => {
-	const [createUserWithEmailAndPassword, user, loading, error] =
-		useCreateUserWithEmailAndPassword(auth);
+	// const [createUserWithEmailAndPassword, user, loading, error] =
+	// 	useCreateUserWithEmailAndPassword(auth);
 	// const [user, loading1, error1] = useAuthState(auth);
 
 	const [email, setEmail] = useState('');
@@ -23,25 +20,97 @@ const Register = () => {
 	const from = location.state?.from?.pathname || '/';
 	console.log(email, password);
 
-	const onsubmit = () => {
+	const onsubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 		if (password === rePassword) {
-			createUserWithEmailAndPassword(email, password);
-			toast('hi');
+			createUserWithEmailAndPassword(auth, email, password)
+				.then((userCredential) => {
+					if (userCredential.user) {
+						navigate(from, { replace: true });
+					}
+				})
+				.catch((error) => {
+					toast.error(error.message);
+					// ..
+				});
 		}
 	};
 
-	// if (loading || loading1) {
-	// 	return <Loading />;
-	// }
-	// if (error || error1) {
-	// 	errorHandeler(error);
-	// }
-	console.log(user);
-	if (user?.user) {
-		navigate(from, { replace: true });
-	}
-
-	return <section className="container-fluid mt-3"></section>;
+	return (
+		<section className="container-fluid mt-3">
+			<form onSubmit={onsubmit} className="w-75 mx-auto card p-5">
+				<h2 className="text-center">Create A User</h2>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Name
+					</label>
+					<input
+						type="text"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Email address
+					</label>
+					<input
+						onBlur={(e) => setEmail(e.target.value)}
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						required
+					/>
+					<div id="emailHelp" className="form-text">
+						We'll never share your email with anyone else.
+					</div>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputPassword1" className="form-label">
+						Password
+					</label>
+					<input
+						onBlur={(e) => setPassword(e.target.value)}
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+						required
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputPassword1" className="form-label">
+						Retype Password
+					</label>
+					<input
+						onBlur={(e) => setRePassword(e.target.value)}
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+						required
+					/>
+				</div>
+				<div className="mb-3 d-lg-flex justify-content-lg-center d-block">
+					<h6>You Are Alrady A User!</h6>
+					<Link to="/login" className="nav-link">
+						<h6 className="ms-2 text-decoration-underline text-primary">
+							Please LogIn.
+						</h6>
+					</Link>
+				</div>
+				<input
+					type="submit"
+					value="Register"
+					className="btn w-lg-25 w-100 fw-bold mx-auto"
+					style={{ backgroundColor: 'turquoise', color: 'white' }}
+				/>
+				<div className="mt-3">
+					<SocialMedia />
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default Register;
